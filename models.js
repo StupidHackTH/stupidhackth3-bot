7105c2b746db32d7fdf57493c5762f48a2cf3354
Glitch (hello-express) <none>
2019-04-16 11:44:35 +0000

    🌚🏟 Checkpoint ./models.js:577339/1318 ./server.js:577339/1522

--- models.js
+++ models.js
@@ -1,17 +1,39 @@
 const base = require('airtable').base('appGku14IaF3SIUts')
-const lock = require('throat')(1)
+const throat = require('throat')
 
 let teams = (() => {
   let cache
+  const lock = throat(1)
+  const table = base('Teams')
   return {
     get() {
-      return cache || (cache = base.select.all())
+      return cache || (cache = table.select().all()).catch(() => { cache = null })
+    },
+    invalidateCache() {
+      cache = null
+    },
+    async transaction(tx) {
+      await lock(async () => {
+        let result
+        try {
+          result = await tx(table)
+          return result
+        } finally {
+          if (result !== false) cache = null
+        }
+      })
     }
   }
 })()
 
 exports.createTeam = async function(members) {
-  await lock(async () => {
-    
+  await teams.transaction(async (table) => {
+    const rows = teams.get()
+    const data = {
+      name: `New Team ${rows.length}`,
+      participants: members.join(',')
+    }
+    await table.create(data)
+    return data
   })
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -18,31 +19,37 @@ app.get('/', function(req, res) {
 
 app.post('/team', async function(req, res, next) {
   try {
-    const out = [req.body.user_id]
-    req.body.text.replace(/<@(U\w+)/g, (a, x) => out.push(x))
-    if (out.length < 3) {
+    const args = req.body.text.split(/\s+/).filter(x => x.trim())
+    if (args[0] === 'invite') {
+      const out = [req.body.user_id]
+      req.body.text.replace(/<@(U\w+)/g, (a, x) => out.push(x))
+      if (out.length < 3) {
+        throw new Error(`Sorry, cannot create team as you need at least 3 members (${out.length} found).`)
+        return
+      }
+      const team = await models.createTeam(out)
       res.json({
-        response_type: 'ephemeral',
-        text: `Sorry, cannot create team as you need at least 3 members (${
-          out.length
-        } found).`
+        response_type: 'in_channel',
+        text: `OK, created team "${team.name}" with ${out
+          .map(x => `<@${x}>`)
+          .join(', ')}.`
       })
-      return
     }
-    const team = await models.createTeam(out)
-    // const data = {
-    //   Name: `New Team ${teamNumber}`,
-    //   Participants: out.join(',')
-    // }
-    // await base('Teams').create(data)
-    // res.json({
-    //   response_type: 'in_channel',
-    //   text: `OK, created team "${data.Name}" with ${out
-    //     .map(x => `<@${x}>`)
-    //     .join(', ')}.`
-    // })
   } catch (err) {
-    next(err)
+    axios.post(process.env.REPORTING_SLACK_WEBHOOK_URL, {
+      text: [
+        `Failed to run command: /team ${req.body.text}`,
+        `Triggered by <@${req.body.user_id}>`,
+        '',
+        '```',
+        String(err && err.stack),
+        '```',
+      ].join('\n')
+    })
+    res.json({
+      response_type: 'ephemeral',
+      text: `Failed -- ${err}`
+    })
   }
 })
 
