f40db87e64c9f6757fdab77359c9ab6e9e676b51
Glitch (hello-express) <none>
2019-04-16 08:46:05 +0000

    🚃🌕 Checkpoint ./server.js:577339/2485

--- server.js
+++ server.js
@@ -17,9 +17,45 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/team', function(req, res) {
-  console.log(req.body);
-  res.json({ });
+const getNextTeamNumber = (() => {
+  let firstPromise
+  let nextNumber
+  async function getTeams() {
+    return (await base('Teams').select().all()).length
+  }
+  return async () => {
+    if (!nextNumber) {
+      const firstNumber = 1 + (await (firstPromise || (firstPromise = getTeams())))
+      if (!nextNumber) nextNumber = firstNumber
+    }
+    return nextNumber++
+  }
+})()
+
+app.post('/team', async function(req, res, next) {
+  try {
+    const teamNumber = await getNextTeamNumber();
+    const out = [req.body.user_id];
+    req.body.text.replace(/<@(U\w+)/g, (a, x) => out.push(x));
+    if (out.length < 3) {
+      res.json({
+        response_type: "ephemeral",
+        text: `Sorry, cannot create team as you need at least 3 members (${out.length} found).`
+      });
+      return
+    }
+    const data = {
+      Name: `New Team ${teamNumber}`,
+      Participants: out.join(','),
+    };
+    await base('Teams').create(data);
+    res.json({
+      response_type: "in_channel",
+      text: `OK, created team "${data.Name}" with ${out.map(x => `<@${x}>`).join(', ')}.`
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 const listener = app.listen(process.env.PORT, function() {
