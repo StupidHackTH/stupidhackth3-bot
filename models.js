ab2a780cdb3c613ec46d2bb93b1dcc323549b9ae
Glitch (hello-express) <none>
2019-04-17 17:22:13 +0000

    ⚡️🐪 Checkpoint ./models.js:577339/3987

--- models.js
+++ models.js
@@ -4,18 +4,19 @@ function table(tableName) {
   const table = base(tableName)
   return {
     get() {
+      console.log('Fetching airtable table ' + tableName)
       return cache || (cache = table.select().all()).catch(() => { cache = null })
     },
     invalidateCache() {
       cache = null
     },
     async transaction(tx) {
-      let result
+      let noop = false
       try {
-        result = await tx(table)
+        const result = await tx(table, () => { noop = true })
         return result
       } finally {
-        if (result !== false) cache = null
+        if (!noop) cache = null
       }
     }
   }
@@ -24,22 +25,22 @@ function table(tableName) {
 const teams = table('Teams')
 
 const findExistingTeam = (teamRecords, userId) => {
-  return teamRecords.find(teamRecord => teamRecord.fields.participants.split(',').includes(userId))
+  return teamRecords.find(teamRecord => (teamRecord.fields.participants || '').split(',').includes(userId))
 }
 
 const formatUsers = userIds => userIds.map(u => `<@${u}>`).join(', ')
 
 exports.addToTeam = async function(requesterId, addeeIds) {
-  return await teams.transaction(async table => {
+  return await teams.transaction(async (table, noop) => {
     const teamRecords = await teams.get()
     const existingTeam = findExistingTeam(teamRecords, requesterId)
+    const addableIds = addeeIds.filter(id => !findExistingTeam(teamRecords, id))
+    const unaddableIds = addeeIds.filter(id => !addableIds.includes(id))
+    const btw = unaddableIds.length
+      ? ` Didn’t add ${formatUsers(unaddableIds)} because they already belong to a team.`
+      : ''
     if (existingTeam) {
       const existingParticipants = existingTeam.fields.participants.split(',')
-      const addableIds = addeeIds.filter(id => !findExistingTeam(teamRecords, id))
-      const unaddableIds = addeeIds.filter(id => !addableIds.includes(id))
-      const btw = unaddableIds.length
-        ? ` Didn’t add ${formatUsers(unaddableIds)} because they already belong to a team.`
-        : ''
       const participants = [...new Set([
         ...existingParticipants,
         ...addableIds,
@@ -51,22 +52,23 @@ exports.addToTeam = async function(requesterId, addeeIds) {
         })
         return `Added ${formatUsers(added)} to team “${existingTeam.fields.name}”.${btw}`
       } else {
+        noop()
         return `Didn’t add anyone new to team “${existingTeam.fields.name}”.${btw}`
       }
     } else {
       const number = teamRecords.length + 1
       const participants = [...new Set([
         requesterId,
-        ...addeeIds
+        ...addableIds
       ])]
       if (participants.length < 3) {
-        throw new Error('You need at least 3 unique members to form a team — ' + participants.length + ' found.')
+        throw new Error('You need at least 3 unique members to form a team — ' + participants.length + ' found.' + btw)
       }
       const createdTeam = await table.create({
         name: `New Team ${number}`,
         participants: participants.join(',')
       })
-      return `Created a new team “${createdTeam.fields.name}” with ${formatUsers(participants)}`
+      return `Created a new team “${createdTeam.fields.name}” with ${formatUsers(participants)}${btw}`
     }
   })
 }
@@ -86,43 +88,26 @@ exports.getTeamInfo = async function(requesterId) {
 }
 
 exports.leaveTeam = async function(requesterId) {
-  return await teams.transaction(async table => {
+  return await teams.transaction(async (table, noop) => {
     const teamRecords = await teams.get()
     const existingTeam = findExistingTeam(teamRecords, requesterId)
-    if (existingTeam) {
-      const existingParticipants = existingTeam.fields.participants.split(',')
-      const addableIds = addeeIds.filter(id => !findExistingTeam(teamRecords, id))
-      const unaddableIds = addeeIds.filter(id => !addableIds.includes(id))
-      const btw = unaddableIds.length
-        ? ` Didn’t add ${formatUsers(unaddableIds)} because they already belong to a team.`
-        : ''
-      const participants = [...new Set([
-        ...existingParticipants,
-        ...addableIds,
-      ])]
-      const added = participants.filter(p => !existingParticipants.includes(p))
-      if (added.length > 0) {
-        await table.update(existingTeam.id, {
-          participants: participants.join(',')
-        })
-        return `Added ${formatUsers(added)} to team “${existingTeam.fields.name}”.${btw}`
-      } else {
-        return `Didn’t add anyone new to team “${existingTeam.fields.name}”.${btw}`
-      }
+    if (!existingTeam) {
+      noop()
+      return 'How can you leave your team when you don’t have one?'
+    }
+    const existingParticipants = existingTeam.fields.participants.split(',')
+    const participants = existingParticipants.filter(id => id !== requesterId)
+    if (participants.length < 3) {
+      await table.update(existingTeam.id, {
+        name: existingTeam.fields.name + ' [disbanded]',
+        participants: ''
+      })
+      return `Your team “${existingTeam.fields.name}” has been disbanded because it has less than 3 members. ${formatUsers(participants)} will have to find a new team.`
     } else {
-      const number = teamRecords.length + 1
-      const participants = [...new Set([
-        requesterId,
-        ...addeeIds
-      ])]
-      if (participants.length < 3) {
-        throw new Error('You need at least 3 unique members to form a team — ' + participants.length + ' found.')
-      }
-      const createdTeam = await table.create({
-        name: `New Team ${number}`,
+      await table.update(existingTeam.id, {
         participants: participants.join(',')
       })
-      return `Created a new team “${createdTeam.fields.name}” with ${formatUsers(participants)}`
+      return `You have left team “${existingTeam.fields.name}”.`
     }
   })
 }
